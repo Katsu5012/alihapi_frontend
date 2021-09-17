6058c05643148c52d7dc27cac5bdd3b9bab8500a
hiroki katsu <sns3655512@gmail.com>
2021-09-17 19:11:51 +0900

    no message

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -27,6 +27,7 @@
     <body>
       <h1 class="title">ALiHaPi</h1>
       <p>今日のちょっと楽しかったことを共有するサイト</p>
+      <P>穏やかな気持ちで過ごせるように</P>
       <h2>機能一覧</h2>
       <div class="Items">
         <div class="Item"></div>
